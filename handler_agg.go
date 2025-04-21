ed03de18f6b92825c387dc8c22d00b4eb51dd53c
Fernando Morato <fernandomorato93@gmail.com>
2025-04-21 02:57:10 -0300

    enhance agg command

--- handler_agg.go
+++ handler_agg.go
@@ -3,19 +3,43 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/fernandomorato/gator/rss"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.Args) != 0 {
-		return fmt.Errorf("usage: cli %s", cmd.Name)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: cli %s <time_between_reqs>", cmd.Name)
 	}
 
-	feed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
 	if err != nil {
-		return fmt.Errorf("error fetching feed: %v", err)
+		return fmt.Errorf("error parsing time_between_reqs: %v", err)
 	}
-	fmt.Println(feed)
+
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenRequests)
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	feedRecord, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("error getting next feed to fetch: %v", err)
+	}
+
+	feed, err := rss.FetchFeed(context.Background(), feedRecord.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed %s: %v", feedRecord.Name, err)
+	}
+	s.db.MarkFeedFetched(context.Background(), feedRecord.ID)
+
+	fmt.Printf("Fetched feed %s. The items are:\n", feedRecord.Name)
+	for _, item := range feed.Channel.Item {
+		fmt.Printf("- %s\n", item.Title)
+	}
 	return nil
 }
